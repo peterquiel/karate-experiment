6d4ee5aed384a92afd53a5502ffdffed6ba6b9e2
pedda <pquiel@wescale.com>
2019-04-19 08:17:02 +0200

    :white_check_mark: add a test to explain relative resource location
    I discovered a problem that the resoruce location relative to a class
    doesn't work with gradle if you don't but your resource file into
    the class file folder.
    
    Gradle separates resources from class files, maven dosn't by default.

--- src/test/groovy/com/github/peterquiel/karate/experiment/CallFeatureFromJavaTest.groovy
+++ src/test/groovy/com/github/peterquiel/karate/experiment/CallFeatureFromJavaTest.groovy
@@ -19,6 +19,11 @@ class CallFeatureFromJavaTest {
     void "calling feature with classpath"() {
         def args = [parameter: "Mr. Pink"]
 
+//        This does not work, because the file look up expects that the feature file resides next to the class file
+//        This is true for maven, but gradle put all resource file into a separate resource folder.
+//        Hence, this does not work and I have to use the `classpath:` lookup instead.
+//        def result = Runner.runFeature(getClass(), "java-api.feature", args, false)
+
         def result = Runner.runFeature("classpath:com/github/peterquiel/karate/experiment/java-api.feature", args, false)
 
         assert result.simpleJson == [foo: 'bar']
